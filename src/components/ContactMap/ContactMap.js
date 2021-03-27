2f89ca29f27d23083eb3aac288566491ba5da6f5
Ghada Mashaqi <ghada.mashaqi.95@gmail.com>
2021-03-27 15:53:29 +0300

    Fix Routes

--- src/components/ContactMap/ContactMap.js
+++ src/components/ContactMap/ContactMap.js
@@ -5,7 +5,7 @@ const ContactMap = (props) => {
     return(
         <React.Fragment>
             <section id="location">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13534.706676624011!2d35.8530068!3d31.9967794!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1bfa417dfc886357!2sQiotic!5e0!3m2!1sen!2siq!4v1615818094095!5m2!1sen!2siq" width="100%" height="500" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13534.706676624011!2d35.8530068!3d31.9967794!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1bfa417dfc886357!2sQiotic!5e0!3m2!1sen!2siq!4v1615818094095!5m2!1sen!2siq" width="100%" height="500" loading="lazy" title="GoogleMap"></iframe>
             </section>
         </React.Fragment>
     )
